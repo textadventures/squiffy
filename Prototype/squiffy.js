1dd370d0f3b538a6745ef8d4e260f9ba86d92747
Alex Warren <alex@textadventures.co.uk>
2013-12-01 15:40:04 +0000

    Add links to new story sections

--- Prototype/squiffy.js
+++ Prototype/squiffy.js
@@ -7,27 +7,53 @@ var squiffy = {
 				if ($(this).hasClass("disabled")) return;
 				var passage = $(this).data("passage");
 				if (passage) {
-					$("#squiffy-output").append(squiffy.story.section.passages[passage].text);
+					squiffy.ui.write(squiffy.story.section.passages[passage].text);
+					$(this).addClass("disabled");
+					return;
+				}
+				var section = $(this).data("section");
+				if (section) {
+					squiffy.story.go(section);
 					$(this).addClass("disabled");
 				}
 			});
 			squiffy.story.go("intro");
 		},
 		go: function(section) {
+			squiffy.ui.newSection();
 			squiffy.story.section = squiffy.story.sections[section];
-			$("#squiffy-output").append(squiffy.story.section.text);
+			squiffy.ui.write(squiffy.story.section.text);
 		},
 		sections: {
 			intro: {
-				text: "Hello world. <a class='squiffy-link' data-passage='test'>Link one</a>.",
+				text: "Hello world. <a class='squiffy-link' data-passage='test'>Link one</a>. <a class='squiffy-link' data-section='section2'>Next section</a>.",
 				passages: {
 					test: {
 						text: "Text for the first link.",
 					},
 				},
 			},
+			section2: {
+				text: "This is section 2."
+			}
 		},
 	},
+	ui: {
+		sectionCount: 0,
+		currentSection: null,
+		newSection: function() {
+			if (squiffy.ui.currentSection) {
+				$(".squiffy-link", squiffy.ui.currentSection).addClass("disabled");
+			}
+			var id = "squiffy-section-" + squiffy.ui.sectionCount++;
+			squiffy.ui.currentSection = $("<div/>", {
+		        id: id,
+		    }).appendTo("#squiffy-output");
+		},
+		write: function(text) {
+			squiffy.ui.currentSection.append(text);
+		}
+	}
 };
 
 $(function(){
