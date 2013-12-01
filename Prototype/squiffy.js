8fc2d2a7eec50d2c00d16ab034f5b89140732801
Alex Warren <alex@textadventures.co.uk>
2013-12-01 15:30:15 +0000

    Add clickable links

--- Prototype/squiffy.js
+++ Prototype/squiffy.js
@@ -3,11 +3,28 @@ var squiffy = {
 		name: "Prototype story",
 		author: "Alex Warren",
 		begin: function () {
-			$("#squiffy-output").append(squiffy.story.sections["intro"].text);
+			$(document).on("click", "a.squiffy-link", function (event) {
+				if ($(this).hasClass("disabled")) return;
+				var passage = $(this).data("passage");
+				if (passage) {
+					$("#squiffy-output").append(squiffy.story.section.passages[passage].text);
+					$(this).addClass("disabled");
+				}
+			});
+			squiffy.story.go("intro");
+		},
+		go: function(section) {
+			squiffy.story.section = squiffy.story.sections[section];
+			$("#squiffy-output").append(squiffy.story.section.text);
 		},
 		sections: {
 			intro: {
-				text: "Hello world."
+				text: "Hello world. <a class='squiffy-link' data-passage='test'>Link one</a>.",
+				passages: {
+					test: {
+						text: "Text for the first link.",
+					},
+				},
 			},
 		},
 	},
