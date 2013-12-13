f6382a0285cbef6c7e22da2a0a71de9045885acc
Alex Warren <alex@axeuk.com>
2013-12-13 21:27:07 +0000

    Starting to implement text processor

--- squiffy.template.js
+++ squiffy.template.js
@@ -87,14 +87,14 @@ var squiffy = {
 			squiffy.set("_section-count", sectionCount);
 			var id = "squiffy-section-" + sectionCount;
 			squiffy.ui.currentSection = $("<div/>", {
-		        id: id,
-		    }).appendTo("#squiffy-output");
-		    squiffy.set("_output-section", id);
+				id: id,
+			}).appendTo("#squiffy-output");
+			squiffy.set("_output-section", id);
 		},
 		write: function(text) {
 			squiffy.ui.screenIsClear = false;
 			squiffy.ui.scrollPosition = $("#squiffy-output").height();
-			squiffy.ui.currentSection.append($("<div/>").html(text));
+			squiffy.ui.currentSection.append($("<div/>").html(squiffy.ui.processText(text)));
 			squiffy.ui.scrollToEnd();
 		},
 		clearScreen: function() {
@@ -103,16 +103,70 @@ var squiffy = {
 			squiffy.ui.newSection();
 		},
 		scrollToEnd: function() {
-		    var scrollTo = squiffy.ui.scrollPosition;
-		    var currentScrollTop = Math.max($("body").scrollTop(), $("html").scrollTop());
-		    if (scrollTo > currentScrollTop) {
-		        var maxScrollTop = $(document).height() - $(window).height();
-		        if (scrollTo > maxScrollTop) scrollTo = maxScrollTop;
-		        var distance = scrollTo - currentScrollTop;
-		        var duration = distance / 0.5;
-		        $("body,html").stop().animate({ scrollTop: scrollTo }, duration, "easeInOutQuad");
-		    }
-		}
+			var scrollTo = squiffy.ui.scrollPosition;
+			var currentScrollTop = Math.max($("body").scrollTop(), $("html").scrollTop());
+			if (scrollTo > currentScrollTop) {
+				var maxScrollTop = $(document).height() - $(window).height();
+				if (scrollTo > maxScrollTop) scrollTo = maxScrollTop;
+				var distance = scrollTo - currentScrollTop;
+				var duration = distance / 0.5;
+				$("body,html").stop().animate({ scrollTop: scrollTo }, duration, "easeInOutQuad");
+			}
+		},
+		processText: function(text) {
+			function process(text, data) {
+				var containsUnprocessedSection = false;
+				var open = text.indexOf("{");
+				var close;
+				
+				if (open > -1) {
+			  		var nestCount = 1;
+				 	var searchStart = open + 1;
+				 	var finished = false;
+				 
+				 	while (!finished) {
+						var nextOpen = text.indexOf("{", searchStart);
+						var nextClose = text.indexOf("}", searchStart);
+			 
+						if (nextClose > -1) {
+					 		if (nextOpen > -1 && nextOpen < nextClose) {
+								nestCount++;
+								searchStart = nextOpen + 1;
+				 			}
+					 		else {
+								nestCount--;
+								searchStart = nextClose + 1;
+								if (nestCount == 0) {
+						 			close = nextClose;
+						 			containsUnprocessedSection = true;
+						 			finished = true;
+								}
+							}
+						}
+						else {
+					 		finished = true;
+						}
+				 	}
+				}
+				
+				if (containsUnprocessedSection) {
+					var section = text.substring(open + 1, close);
+					var value = processTextCommand(section, data);
+					text = text.substring(0, open) + value + process(text.substring(close + 1), data);
+				}
+				
+				return (text)
+			}
+
+			function processTextCommand(text, data) {
+				return "--(" + text + ")--";
+			}
+
+			var data = {
+				fulltext: text
+			};
+			return process(text, data);
+		},
 	},
 	set: function(attribute, value) {
 		if (!squiffy.util.isStorageSupported()) return;
@@ -127,10 +181,10 @@ var squiffy = {
 	util: {
 		isStorageSupported: function() {
 			try {
-		        return 'localStorage' in window && window['localStorage'] !== null;
-		    } catch(e) {
-		        return false;
-		    }
+				return 'localStorage' in window && window['localStorage'] !== null;
+			} catch(e) {
+				return false;
+			}
 		}
 	}
 };
