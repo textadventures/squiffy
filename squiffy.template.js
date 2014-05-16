b813879287fb43922a55a4e3ede2208feb2d2a2f
Alex Warren <alex@textadventures.co.uk>
2014-05-16 16:20:00 +0200

    Save after replace transition

--- squiffy.template.js
+++ squiffy.template.js
@@ -117,7 +117,9 @@ var squiffy = {
 			var $labels = $(".squiffy-label-" + label);
 			$labels.fadeOut(1000, function() {
 				$labels.html(squiffy.ui.processText(text));
-				$labels.fadeIn(1000);
+				$labels.fadeIn(1000, function() {
+					squiffy.story.save();
+				});
 			});
 		},
 		go: function(section) {
