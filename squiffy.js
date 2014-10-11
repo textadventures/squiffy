ef4a169940757761c37d189a21de9d945d146734
Alex Warren <alex@textadventures.co.uk>
2014-10-11 14:00:07 +0100

    Converting Python to JS

--- squiffy.js
+++ squiffy.js
@@ -1,17 +1,107 @@
 #!/usr/bin/env node
 
 var _ = require("underscore");
+var path = require("path");
 
 var squiffy_version = "2.0";
 
 console.log("Squiffy " + squiffy_version);
 
-var compiler = {
-	process: function(inputFilename, sourcePath, options) {
-		console.log("inputFilename:" + inputFilename);
-		console.log("sourcePath:" + sourcePath);
-		console.log(options);
+function Compiler() {
+	this.process = function(inputFilename, sourcePath, options) {
+		var outputPath = path.resolve(path.dirname(inputFilename));
+
+		var story = new Story();
+		story.set_id(path.resolve(inputFilename));
+
+		var success = this.processFile(story, path.resolve(inputFilename), true);
+
+	    if (!success) {
+	        console.log("Failed.");
+	        return;
+		}
 	}
+
+	this.processFile = function(story, inputFilename, isFirst) {
+		if (_.contains(story.files, inputFilename)) {
+			return true;
+		}
+
+		story.files.push(inputFilename)
+    	console.log("Loading " + inputFilename);
+
+    	return true;
+	}
+}
+
+function Story() {
+    this.sections = {};
+    this.title = "";
+    this.scripts = [];
+    this.files = [];
+    this.start = "";
+
+    this.addSection = function(name, filename, line) {
+        section = new Section(name, filename, line);
+        this.sections[name] = section;
+        return section;
+    }
+
+    this.set_id = function(filename) {
+    	// TODO
+        //file_id = str(uuid.getnode()) + filename
+        //self.id = hashlib.sha1(file_id.encode('utf-8')).hexdigest()[0:10]
+    }
+}
+
+function Section(name, filename, line) {
+    this.name = name;
+    this.filename = filename;
+    this.line = line;
+    this.text = [];
+    this.passages = {};
+    this.js = [];
+    this.clear = false;
+    this.attributes = [];
+
+    this.addPassage = function(name, line) {
+        passage = new Passage(name, line);
+        this.passages[name] = passage;
+        return passage;
+    }
+
+    this.addText = function(text) {
+        this.text.push(text);
+    }
+
+    this.addJS = function(text) {
+        this.js.push(text);
+    }
+
+    this.addAttribute = function(text) {
+        this.attributes.push(text);
+    }
+}
+
+function Passage(name, line) {
+	this.name = name;
+	this.line = line;
+	this.text = [];
+	this.js = [];
+	this.clear = false;
+	this.attributes = [];
+
+    this.addText = function(text) {
+        this.text.push(text);
+    }
+
+    this.addJS = function(text) {
+        this.js.push(text);
+    }
+
+    this.addAttribute = function(text) {
+        this.attributes.push(text);
+    }
 }
 
 function getOptions() {
@@ -27,5 +117,6 @@ if (process.argv.length < 3) {
 }
 else {
 	var options = getOptions();
+	var compiler = new Compiler();
 	compiler.process(process.argv[2], __dirname, options);
 }
