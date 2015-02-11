9d81dd8dd53144150bc8bebbd8727efa394eaaae
Alex Warren <alex@textadventures.co.uk>
2015-02-11 21:56:47 +0000

    Indentation tidy

--- squiffy.js
+++ squiffy.js
@@ -11,7 +11,7 @@ var crypto = require('crypto');
 String.prototype.format = function() {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined' ? args[number] : match;
+        return typeof args[number] != 'undefined' ? args[number] : match;
     });
 };
 
