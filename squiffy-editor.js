d7cd0c8eaa6c07191c3710c20b54e040bdc42387
Alex Warren <alex@textadventures.co.uk>
2015-02-14 17:07:48 +0000

    Single quotes

--- squiffy-editor.js
+++ squiffy-editor.js
@@ -1,37 +1,42 @@
-(function (){
+/* jshint quotmark: single */
+/* jshint evil: true */
+
+(function () {
+    'use strict';
+
     $.fn.squiffyEditor = function (options) {
         var editorHtml = this;
-        $.get("squiffy-editor.html", function (data) {
+        $.get('squiffy-editor.html', function (data) {
             editorHtml.html(data);
             
-            var editor = ace.edit("editor");
-            editor.setTheme("ace/theme/eclipse");
-            editor.getSession().setMode("ace/mode/markdown");
+            var editor = ace.edit('editor');
+            editor.setTheme('ace/theme/eclipse');
+            editor.getSession().setMode('ace/mode/markdown');
             editor.getSession().setUseWrapMode(true);
             editor.focus();
 
             editor.setValue(options.data, -1);
 
-            $("#run").click(function () {
-                $("#output-container").html("");
-                $.post("http://squiffy.textadventures.co.uk", editor.getValue(), function (data) {
-                    $("<div/>", { id: "output", style: "max-height: 400px" })
-                        .appendTo("#output-container");
+            $('#run').click(function () {
+                $('#output-container').html('');
+                $.post('http://squiffy.textadventures.co.uk', editor.getValue(), function (data) {
+                    $('<div/>', { id: 'output', style: 'max-height: 400px' })
+                        .appendTo('#output-container');
 
-                    $("<hr/>").appendTo("#output-container");
-                    $("<button/>", { id: "sample-restart", "class": "btn btn-primary btn-sm" })
-                        .html("Restart")
-                        .appendTo("#output-container");
+                    $('<hr/>').appendTo('#output-container');
+                    $('<button/>', { id: 'sample-restart', 'class': 'btn btn-primary btn-sm' })
+                        .html('Restart')
+                        .appendTo('#output-container');
 
-                    if (data.indexOf("Failed") === 0) {
-                        $("#output").html(data);
+                    if (data.indexOf('Failed') === 0) {
+                        $('#output').html(data);
                         return;
                     }
 
                     eval(data);
-                    $("#output").squiffy({
-                        restart: "#sample-restart",
-                        scroll: "element",
+                    $('#output').squiffy({
+                        restart: '#sample-restart',
+                        scroll: 'element',
                         persist: false,
                         restartPrompt: false
                     });
