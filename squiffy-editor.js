6ec5c49b1eb9cc94af4f2d0ce0bc3e531bdb0de3
Alex Warren <alex@textadventures.co.uk>
2015-02-14 17:05:37 +0000

    Add functionality from ScratchPad

---
+++ squiffy-editor.js
@@ -0,0 +1,42 @@
+(function (){
+    $.fn.squiffyEditor = function (options) {
+        var editorHtml = this;
+        $.get("squiffy-editor.html", function (data) {
+            editorHtml.html(data);
+            
+            var editor = ace.edit("editor");
+            editor.setTheme("ace/theme/eclipse");
+            editor.getSession().setMode("ace/mode/markdown");
+            editor.getSession().setUseWrapMode(true);
+            editor.focus();
+
+            editor.setValue(options.data, -1);
+
+            $("#run").click(function () {
+                $("#output-container").html("");
+                $.post("http://squiffy.textadventures.co.uk", editor.getValue(), function (data) {
+                    $("<div/>", { id: "output", style: "max-height: 400px" })
+                        .appendTo("#output-container");
+
+                    $("<hr/>").appendTo("#output-container");
+                    $("<button/>", { id: "sample-restart", "class": "btn btn-primary btn-sm" })
+                        .html("Restart")
+                        .appendTo("#output-container");
+
+                    if (data.indexOf("Failed") === 0) {
+                        $("#output").html(data);
+                        return;
+                    }
+
+                    eval(data);
+                    $("#output").squiffy({
+                        restart: "#sample-restart",
+                        scroll: "element",
+                        persist: false,
+                        restartPrompt: false
+                    });
+                });
+            });
+        });
+    };
+})();
