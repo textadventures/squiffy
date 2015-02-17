01f1632cc9d89ad4719c1d5e1f40400a513c3fea
Alex Warren <alex@textadventures.co.uk>
2015-02-17 22:28:12 +0000

    Move editor html into JS file

--- squiffy-editor.js
+++ squiffy-editor.js
@@ -68,51 +68,49 @@
 
     var methods = {
         init: function (options) {
-            var editorHtml = this;
+            var element = this;
             settings = options;
-            
-            $.get('squiffy-editor.html', function (data) {
-                editorHtml.html(data);
-                editorHtml.layout({
-                    applyDefaultStyles: true,
-                    onresize: function () {
-                        if (editor) editor.resize();
-                    },
-                    north__resizable: false,
-                    north__closable: false,
-                    north__spacing_open: 0,
-                    east__size: 0.5,
-                    south__size: 80,
-                });
-                
-                editor = ace.edit('editor');
-                editor.setTheme('ace/theme/eclipse');
-                editor.getSession().setMode('ace/mode/markdown');
-                editor.getSession().setUseWrapMode(true);
-                editor.getSession().on('change', editorChange);
-                editor.focus();
 
-                editor.getSession().setValue(options.data, -1);
-
-                if (options.open) {
-                    $('#open').show();
-                    $('#open').click(options.open);
-                }
-
-                if (options.save) {
-                    $('#save').show();
-                    $('#save').click(options.save);
-                }
-
-                $('#run').click(run);
-                $('#restart').click(restart);
+            element.html(editorHtml);
+            element.layout({
+                applyDefaultStyles: true,
+                onresize: function () {
+                    if (editor) editor.resize();
+                },
+                north__resizable: false,
+                north__closable: false,
+                north__spacing_open: 0,
+                east__size: 0.5,
+                south__size: 80,
             });
+            
+            editor = ace.edit('editor');
+            editor.setTheme('ace/theme/eclipse');
+            editor.getSession().setMode('ace/mode/markdown');
+            editor.getSession().setUseWrapMode(true);
+            editor.getSession().on('change', editorChange);
+            editor.focus();
+
+            editor.getSession().setValue(options.data, -1);
+
+            if (options.open) {
+                $('#open').show();
+                $('#open').click(options.open);
+            }
+
+            if (options.save) {
+                $('#save').show();
+                $('#save').click(options.save);
+            }
+
+            $('#run').click(run);
+            $('#restart').click(restart);
         },
         load: function (data) {
             editor.getSession().setValue(data, -1);
         },
         setStorageKey: function (key) {
-            settings.storageKey = key
+            settings.storageKey = key;
         },
     };
 
@@ -133,4 +131,37 @@
 
         $('<p/>').html('{0} = {1}'.format(attribute, value)).appendTo('#debugger');
     };
+
+    var editorHtml = 
+        '<div class="ui-layout-north">\n' +
+            '<button id="open" class="btn btn-primary" style="display: none"><span class="glyphicon glyphicon-open-file"></span> Open</button>\n' +
+            '<button id="save" class="btn btn-primary" style="display: none"><span class="glyphicon glyphicon-save"></span> Save</button>\n' +
+            '<button id="run" class="btn btn-success"><span class="glyphicon glyphicon-play"></span> Run</button>\n' +
+            '<button id="restart" class="btn btn-success" style="display: none"><span class="glyphicon glyphicon-refresh"></span> Restart</button>\n' +
+            '<div style="float: right">\n' +
+                '<div class="btn-group">\n' +
+                        '<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">\n' +
+                                '<span class="glyphicon glyphicon-cloud-download"></span>\n' +
+                                'Download\n' +
+                                '<span class="caret"></span>\n' +
+                        '</button>\n' +
+                        '<ul class="dropdown-menu" role="menu">\n' +
+                                '<li><a href="#">Squiffy script</a></li>\n' +
+                                '<li><a href="#">Export HTML and JavaScript</a></li>\n' +
+                                '<li><a href="#">Export JavaScript only</a></li>\n' +
+                        '</ul>\n' +
+                '</div>\n' +
+                '<button id="preview" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span> Preview</button>\n' +
+                '<button id="publish" class="btn btn-primary"><span class="glyphicon glyphicon-circle-arrow-up"></span> Publish</button>\n' +
+            '</div>\n' +
+        '</div>\n' +
+        '<div class="ui-layout-center">\n' +
+            '<div id="editor"></div>\n' +
+        '</div>\n' +
+        '<div class="ui-layout-east">\n' +
+            '<div id="output-container"></div>\n' +
+        '</div>\n' +
+        '<div class="ui-layout-south">\n' +
+            '<div id="debugger"></div>\n' +
+        '</div>\n';
 })();
