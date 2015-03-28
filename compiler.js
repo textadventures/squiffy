f7a6d5df81b08a0ce539d89fddd2333989f68ab5
Abdul Rohman <abdul.rohman@wyel.com.sg>
2015-03-28 22:58:56 +0700

    add separate link classes for issue #30

--- compiler.js
+++ compiler.js
@@ -368,7 +368,7 @@
             var links = this.allMatchesForGroup(input, namedSectionLinkRegex, 2);
             this.checkSectionLinks(story, links, section, passage);
 
-            input = input.replace(namedSectionLinkRegex, '<a class="squiffy-link" data-section="$2">$1</a>');
+            input = input.replace(namedSectionLinkRegex, '<a class="squiffy-link link-section" data-section="$2">$1</a>');
 
             // namedPassageLinkRegex matches:
             //   open [
@@ -382,7 +382,7 @@
             links = this.allMatchesForGroup(input, namedPassageLinkRegex, 2);
             this.checkPassageLinks(story, links, section, passage);
 
-            input = input.replace(namedPassageLinkRegex, '<a class="squiffy-link" data-passage="$2">$1</a>');
+            input = input.replace(namedPassageLinkRegex, '<a class="squiffy-link link-passage" data-passage="$2">$1</a>');
 
             // unnamedSectionLinkRegex matches:
             //   open [[
@@ -393,7 +393,7 @@
             links = this.allMatchesForGroup(input, unnamedSectionLinkRegex, 1);
             this.checkSectionLinks(story, links, section, passage);
 
-            input = input.replace(unnamedSectionLinkRegex, '<a class="squiffy-link" data-section="$1">$1</a>');
+            input = input.replace(unnamedSectionLinkRegex, '<a class="squiffy-link link-section" data-section="$1">$1</a>');
 
             // unnamedPassageLinkRegex matches:
             //   open [
@@ -405,7 +405,7 @@
             links = this.allMatchesForGroup(input, unnamedPassageLinkRegex, 1);
             this.checkPassageLinks(story, links, section, passage);
 
-            input = input.replace(unnamedPassageLinkRegex, '<a class="squiffy-link" data-passage="$1">$1</a>$2');
+            input = input.replace(unnamedPassageLinkRegex, '<a class="squiffy-link link-passage" data-passage="$1">$1</a>$2');
 
             return marked(input).trim();
         };
