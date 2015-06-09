33918fe64de4471524ed6135eb742538b467b111
Alex Warren <alex@textadventures.co.uk>
2015-06-09 21:15:47 +0100

    Use atom

--- nw/test.js
+++
@@ -1,6 +0,0 @@
-if (process.platform =='darwin') {
-    var gui = require('nw.gui');
-    var mb = new gui.Menu({type:'menubar'});
-    mb.createMacBuiltin('Squiffy');
-    gui.Window.get().menu = mb;
-}
