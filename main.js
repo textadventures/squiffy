908c9c62062382bc99fbeaa7d180e9cf62ec0438
Alex Warren <alex@textadventures.co.uk>
2015-07-01 21:06:56 +0100

    Set icon
    Only required for Linux version, it seems

--- main.js
+++ main.js
@@ -26,7 +26,11 @@ var mainWindow = null;
 
 var init = function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1200, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    icon: __dirname + '/squiffy.png'
+  });
 
   mainWindow.openFile = openFile;
 
