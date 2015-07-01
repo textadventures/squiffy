1a6b9fd7daef5587dc07a4aff7e1cfcc8a6c8f43
Alex Warren <alex@textadventures.co.uk>
2015-07-01 21:11:37 +0100

    Add Linux build script

---
+++ build-linux.sh
@@ -0,0 +1,1 @@
+electron-packager ./ Squiffy --platform=linux --arch=x64 --version=0.28.3 --app-bundle-id=uk.co.textadventures.squiffy --helper-bundle-id=uk.co.textadventures.squiffy.helper --app-version=3.9.0
