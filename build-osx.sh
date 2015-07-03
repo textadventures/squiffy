6ffac6960cff066d80655e963c154de32cb47200
Alex Warren <alex@textadventures.co.uk>
2015-07-03 11:45:28 +0100

    OS X build script

---
+++ build-osx.sh
@@ -0,0 +1,7 @@
+./build-common.sh
+
+rm -r Squiffy.app
+
+electron-packager ./build Squiffy --platform=darwin --arch=x64 --version=0.27.3 --app-bundle-id=uk.co.textadventures.squiffy --helper-bundle-id=uk.co.textadventures.squiffy.helper --icon=squiffy.icns --app-version=3.9.0
+cp file\ association\ -\ Info.plist Squiffy.app/Contents/Info.plist
+/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -lint -f Squiffy.app
