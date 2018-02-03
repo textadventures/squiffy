f35a71b0a6b82026439f865dbe0aca87f4c28fa7
baltasarq <baltasarq@gmail.com>
2018-02-03 11:08:30 +0100

    Fix for issue #9

--- storage.js
+++ storage.js
@@ -1,51 +1,65 @@
-// based on http://www.mylifeforthecode.com/saving-and-restoring-window-state-in-electron/
+// Squiffy-editor MIT License
+// based on
+// http://www.mylifeforthecode.com/saving-and-restoring-window-state-in-electron/
 
 var app;
 
 try {
-  app = require('electron').remote.app; 
+  app = require('electron').remote.app;
+} catch (e) {
+  app = require('electron').app;
 }
-catch (e) {
-  app = require('electron').app; 
-}
- 
-var fs = require('fs');
-var path = require('path');
+
+const {dialog} = require('electron')
+const fs = require('fs');
+const path = require('path');
+const dataFilePath = path.join(app.getPath('userData'), 'settings.json');
 var data = null;
-var dataFilePath = path.join(app.getPath('userData'), 'settings.json'); 
 
 function load() {
   if (!fs.existsSync(dataFilePath)) {
     data = {};
+    save();
     return;
   }
 
-  data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8')); 
+  try {
+      data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+  } catch(exc) {
+      const msg = 'Error loading settings. Back to defaults.';
+      data = {}
+      save();
+
+      console.log(msg + "\n\t" + exc);
+      dialog.showMessageBox({
+          message: msg,
+          buttons: ["OK"] });
+  }
 }
 
 function save() {
-  fs.writeFileSync(dataFilePath, JSON.stringify(data)); 
+  fs.writeFileSync(dataFilePath, JSON.stringify(data));
 }
 
 exports.set = function (key, value) {
   load();
-  data[key] = value; 
+  data[key] = value;
   save();
 };
 
-exports.get = function (key) { 
+exports.get = function (key) {
   load();
   var value = null;
   if (key in data) {
     value = data[key];
-  } 
+  }
   return value;
 };
 
-exports.unset = function (key) { 
+exports.unset = function (key) {
   load();
   if (key in data) {
     delete data[key];
     save();
-  } 
+  }
 };
