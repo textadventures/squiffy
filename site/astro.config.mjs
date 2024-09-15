e0adcf1c67e752ad07b95bf2a9775428b5877dcc
Alex Warren <alex@alexwarren.uk>
2024-09-15 12:29:39 +0100

    Adding Docs homepage

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mdx()],
+});
--- site/src/pages/docs/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: "@layouts/MarkdownLayout.astro"
-title: "Squiffy - Documentation"
----
-
-# Docs...
