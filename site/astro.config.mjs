bab1c4298f6e35445770b4d7a5fc5a7422ba108d
Alex Warren <alex@alexwarren.uk>
2024-09-19 19:00:53 +0100

    Trailing slash to match Netlify

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+    trailingSlash: "always",
     integrations: [mdx()],
     markdown: {
         shikiConfig: {
