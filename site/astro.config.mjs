9601ecd4815a8f01e6e29b77c8588a62235519d4
Alex Warren <alex@alexwarren.uk>
2025-08-11 15:20:01 +0100

    Add starlight

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
+import starlight from '@astrojs/starlight';
+import { astroExpressiveCode } from "@astrojs/starlight/expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
     trailingSlash: "always",
-    integrations: [mdx()],
+    integrations: [
+        astroExpressiveCode(),
+        mdx(),
+        starlight({
+            title: 'Squiffy'
+        })],
     markdown: {
         shikiConfig: {
             theme: 'catppuccin-latte'
