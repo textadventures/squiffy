31a9248c2fb41e52792217c899f7398cfb6516ab
Alex Warren <alex@alexwarren.uk>
2025-08-15 17:32:31 +0100

    Move types

---
+++ runtime/src/types.ts
@@ -0,0 +1,61 @@
+export interface SquiffyInitOptions {
+    element: HTMLElement;
+    story: Story;
+    scroll?: string,
+    persist?: boolean,
+    onSet?: (attribute: string, value: any) => void,
+}
+
+export interface SquiffySettings {
+    scroll: string,
+    persist: boolean,
+    onSet: (attribute: string, value: any) => void,
+}
+
+export interface SquiffyApi {
+    restart: () => void;
+    get: (attribute: string) => any;
+    set: (attribute: string, value: any) => void;
+    clickLink: (link: HTMLElement) => boolean;
+    update: (story: Story) => void;
+}
+
+// Previous versions of Squiffy had "squiffy", "get" and "set" as globals - we now pass these directly into JS functions.
+// We may tidy up this API at some point, though that would be a breaking change.
+interface SquiffyJsFunctionApi {
+    get: (attribute: string) => any;
+    set: (attribute: string, value: any) => void;
+    ui: {
+        transition: (f: any) => void;
+    };
+    story: {
+        go: (section: string) => void;
+    };
+}
+
+export interface Story {
+    js: ((
+        squiffy: SquiffyJsFunctionApi,
+        get: (attribute: string) => any,
+        set: (attribute: string, value: any) => void
+    ) => void)[];
+    start: string;
+    id?: string | null;
+    sections: Record<string, Section>;
+}
+
+export interface Section {
+    text?: string;
+    clear?: boolean;
+    attributes?: string[],
+    jsIndex?: number;
+    passages?: Record<string, Passage>;
+    passageCount?: number;
+}
+
+export interface Passage {
+    text?: string;
+    clear?: boolean;
+    attributes?: string[];
+    jsIndex?: number;
+}
