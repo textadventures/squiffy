b868303e90b1911af2297317884ef89cab6e842a
Alex Warren <alex@alexwarren.uk>
2025-08-23 19:56:12 +0100

    Remove old text processor

--- runtime/src/textProcessor.ts
+++ runtime/src/textProcessor.ts
@@ -1,11 +1,9 @@
 import * as marked from 'marked';
 import Handlebars from "handlebars";
-import { startsWith, rotate } from "./utils.js";
+import { rotate } from "./utils.js";
 import { Section } from "./types.js";
 
 export class TextProcessor {
-    enableLegacyTextProcessor = false;
-
     get: (attribute: string) => any;
     set: (attribute: string, value: any) => void;
     story: any;
@@ -78,49 +76,8 @@ export class TextProcessor {
     }
 
     process(text: string, data: any, inline: boolean) {
-        if (this.enableLegacyTextProcessor) {
-            let containsUnprocessedSection = false;
-            const open = text.indexOf('{');
-            let close;
-
-            if (open > -1) {
-                let nestCount = 1;
-                let searchStart = open + 1;
-                let finished = false;
-
-                while (!finished) {
-                    const nextOpen = text.indexOf('{', searchStart);
-                    const nextClose = text.indexOf('}', searchStart);
-
-                    if (nextClose > -1) {
-                        if (nextOpen > -1 && nextOpen < nextClose) {
-                            nestCount++;
-                            searchStart = nextOpen + 1;
-                        } else {
-                            nestCount--;
-                            searchStart = nextClose + 1;
-                            if (nestCount === 0) {
-                                close = nextClose;
-                                containsUnprocessedSection = true;
-                                finished = true;
-                            }
-                        }
-                    } else {
-                        finished = true;
-                    }
-                }
-            }
-
-            if (containsUnprocessedSection) {
-                const section = text.substring(open + 1, close);
-                const value = this.processTextCommand(section, data);
-                text = text.substring(0, open) + value + this.process(text.substring(close! + 1), data, true);
-            }
-        }
-        else {
-            const template = this.handlebars.compile(text);
-            text = template({});
-        }
+        const template = this.handlebars.compile(text);
+        text = template({});
 
         if (inline) {
             return marked.parseInline(text, { async: false }).trim();
@@ -128,117 +85,5 @@ export class TextProcessor {
         else {
             return marked.parse(text, { async: false }).trim();
         }
-    }
-
-    processTextCommand(text: string, data: any) {
-        const currentSection = this.getCurrentSection();
-        if (startsWith(text, 'if ')) {
-            return this.processTextCommand_If(text, data);
-        } else if (startsWith(text, 'else:')) {
-            return this.processTextCommand_Else(text, data);
-        } else if (startsWith(text, 'label:')) {
-            return this.processTextCommand_Label(text, data);
-        } else if (/^rotate[: ]/.test(text)) {
-            return this.processTextCommand_Rotate('rotate', text);
-        } else if (/^sequence[: ]/.test(text)) {
-            return this.processTextCommand_Rotate('sequence', text);
-        } else if (currentSection.passages && text in currentSection.passages) {
-            return this.process(currentSection.passages[text].text || '', data, false);
-        } else if (text in this.story.sections) {
-            return this.process(this.story.sections[text].text || '', data, false);
-        } else if (startsWith(text, '@') && !startsWith(text, '@replace')) {
-            this.processAttributes(text.substring(1).split(","));
-            return "";
-        }
-        return this.get(text);
-    }
-
-    processTextCommand_If(section: string, data: any) {
-        const command = section.substring(3);
-        const colon = command.indexOf(':');
-        if (colon == -1) {
-            return ('{if ' + command + '}');
-        }
-
-        const text = command.substring(colon + 1);
-        let condition = command.substring(0, colon);
-        condition = condition.replace("<", "&lt;");
-        const operatorRegex = /([\w ]*)(=|&lt;=|&gt;=|&lt;&gt;|&lt;|&gt;)(.*)/;
-        const match = operatorRegex.exec(condition);
-
-        let result = false;
-
-        if (match) {
-            const lhs = this.get(match[1]);
-            const op = match[2];
-            let rhs = match[3];
-
-            if (startsWith(rhs, '@')) rhs = this.get(rhs.substring(1));
-
-            if (op == '=' && lhs == rhs) result = true;
-            if (op == '&lt;&gt;' && lhs != rhs) result = true;
-            if (op == '&gt;' && lhs > rhs) result = true;
-            if (op == '&lt;' && lhs < rhs) result = true;
-            if (op == '&gt;=' && lhs >= rhs) result = true;
-            if (op == '&lt;=' && lhs <= rhs) result = true;
-        } else {
-            let checkValue = true;
-            if (startsWith(condition, 'not ')) {
-                condition = condition.substring(4);
-                checkValue = false;
-            }
-
-            if (startsWith(condition, 'seen ')) {
-                result = (this.seen(condition.substring(5)) == checkValue);
-            } else {
-                let value = this.get(condition);
-                if (value === null) value = false;
-                result = (value == checkValue);
-            }
-        }
-
-        const textResult = result ? this.process(text, data, true) : '';
-
-        data.lastIf = result;
-        return textResult;
-    }
-
-    processTextCommand_Else(section: string, data: any) {
-        if (!('lastIf' in data) || data.lastIf) return '';
-        const text = section.substring(5);
-        return this.process(text, data, true);
-    }
-
-    processTextCommand_Label(section: string, data: any) {
-        const command = section.substring(6);
-        const eq = command.indexOf('=');
-        if (eq == -1) {
-            return ('{label:' + command + '}');
-        }
-
-        const text = command.substring(eq + 1);
-        const label = command.substring(0, eq);
-
-        return '<span class="squiffy-label-' + label + '">' + this.process(text, data, true) + '</span>';
-    }
-
-    processTextCommand_Rotate(type: string, section: string) {
-        let options;
-        let attribute = '';
-        if (section.substring(type.length, type.length + 1) == ' ') {
-            const colon = section.indexOf(':');
-            if (colon == -1) {
-                return '{' + section + '}';
-            }
-            options = section.substring(colon + 1);
-            attribute = section.substring(type.length + 1, colon);
-        } else {
-            options = section.substring(type.length + 1);
-        }
-        const rotation = rotate(options.replace(/"/g, '&quot;').replace(/'/g, '&#39;'), null);
-        if (attribute) {
-            this.set(attribute, rotation[0]);
-        }
-        return '<a class="squiffy-link" data-' + type + '="' + rotation[1] + '" data-attribute="' + attribute + '" role="link">' + rotation[0] + '</a>';
     }
 }
