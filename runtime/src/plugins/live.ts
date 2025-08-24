e3cbf912efb3dc488b4cfc0106552b9f78f73eac
Alex Warren <alex@alexwarren.uk>
2025-08-24 19:41:37 +0100

    Implement {{live}}

--- runtime/src/plugins/live.ts
+++ runtime/src/plugins/live.ts
@@ -19,32 +19,23 @@ export const LivePlugin: SquiffyPlugin = {
             return new Handlebars.SafeString(`<span class="squiffy-live" data-attribute="${attribute}">${squiffy.get(attribute)}</span>`);
         });
 
-        function processElement(el: HTMLElement) {
-            // TODO: Update these elements when the attribute changes
-            console.log("Found element:", el);
-        }
-
-        // Handle any already-existing elements
-        squiffy.outputElement.querySelectorAll(".squiffy-live").forEach(processElement);
-
-        // Watch for new ones
-        new MutationObserver((mutations) => {
-            for (const m of mutations) {
-                m.addedNodes.forEach(node => {
-                    if (node instanceof HTMLElement) {
-                        // Check the added node itself
-                        if (node.classList.contains("squiffy-live")) {
-                            processElement(node);
-                        }
-                        // Check any children of the added node
-                        node.querySelectorAll?.(".squiffy-live").forEach(processElement);
-                    }
-                });
-            }
-        }).observe(squiffy.outputElement, {childList: true, subtree: true});
-
         squiffy.on('set', e => {
-            console.log(`Set...${e.attribute} = ${e.value}`);
+            const selector = `[data-attribute="${CSS.escape(e.attribute)}"]`;
+            document.querySelectorAll<HTMLElement>(selector).forEach((el) => {
+                if (el.dataset.section) {
+                    const sectionText = squiffy.getSectionText(el.dataset.section);
+                    if (sectionText) {
+                        el.innerHTML = squiffy.processText(sectionText, true);
+                    }
+                } else if (el.dataset.passage) {
+                    const passageText = squiffy.getPassageText(el.dataset.passage);
+                    if (passageText) {
+                        el.innerHTML = squiffy.processText(passageText, true);
+                    }
+                } else {
+                    el.textContent = e.value;
+                }
+            });
         })
     }
 }
