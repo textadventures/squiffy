7a3c00c0e6cbf472180b8680427b0f34ce2fa822
Alex Warren <alex@alexwarren.uk>
2026-01-03 19:06:13 +0000

    Update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig(
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+    {
+        files: ["eslint.config.*"],
+        languageOptions: {
+            globals: {
+                URL: "readonly",
+            },
+        },
+    },
+    includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+    globalIgnores(["**/.astro/**", "examples/**"]),
     eslint.configs.recommended,
     tseslint.configs.recommended,
-);
+]);
