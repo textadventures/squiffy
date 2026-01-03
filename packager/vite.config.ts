654dc6deecf44f5c7f0b8fdeb35a3af137a3d261
Alex Warren <alex@alexwarren.uk>
2026-01-03 12:12:21 +0000

    Create sourcemap for packager

--- packager/vite.config.ts
+++ packager/vite.config.ts
@@ -4,6 +4,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
     build: {
         target: "es2020",
+        sourcemap: true,
         minify: "esbuild",
         lib: {
             entry: "src/packager.ts",
