f12fd54b46d7b0304901e265836b8f0c2d3d2f39
Alex Warren <alex@alexwarren.uk>
2026-01-12 18:23:52 +0000

    Various enhancements (#147)

--- runtime/src/plugins/replaceLabel.ts
+++ runtime/src/plugins/replaceLabel.ts
@@ -1,19 +1,24 @@
 import { PluginHost, SquiffyPlugin } from "../types.plugins.js";
 import Handlebars from "handlebars";
+import * as marked from "marked";
 import { fadeReplace } from "../utils.js";
 
 export function ReplaceLabel() : SquiffyPlugin {
     return {
         name: "replaceLabel",
         init(squiffy: PluginHost) {
-            squiffy.registerHelper("label", (name: string, options) => {
+            squiffy.registerHelper("label", function(name: string, options) {
                 return new Handlebars.SafeString(`<span class="squiffy-label-${name}">${options.fn(this)}</span>`);
             });
-            squiffy.registerHelper("replace", (name: string, options) => {
+            squiffy.registerHelper("replace", function(name: string, options) {
                 const result = options.fn(this);
                 const element = squiffy.outputElement.querySelector<HTMLElement>(`.squiffy-label-${name}`);
                 if (element) {
-                    squiffy.addTransition(() => fadeReplace(element, result));
+                    const isBlock = result.includes("\n\n");
+                    const html = isBlock
+                        ? marked.parse(result, { async: false }).trim()
+                        : marked.parseInline(result, { async: false }).trim();
+                    squiffy.addTransition(() => fadeReplace(element, html as string));
                 }
                 return "";
             });
