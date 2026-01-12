831a56774213651a998bc12cb87848b8768aec7a
Alex Warren <alex@alexwarren.uk>
2026-01-12 19:42:41 +0000

    Add examples page to docs (#149)

---
+++ site/src/examples.ts
@@ -0,0 +1,5 @@
+// Central location for importing example Squiffy scripts
+// Uses Vite's ?raw suffix to import file contents as strings
+
+export { default as coffeeshop } from "../../examples/coffeeshop/coffeeshop.squiffy?raw";
+export { default as gameshow } from "../../examples/gameshow/gameshow.squiffy?raw";
